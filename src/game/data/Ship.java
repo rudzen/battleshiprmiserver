0e688a76afacf64951cee3fed0a5b75abab0395c
Rudy Kohn <s133235@student.dtu.dk>
2016-04-18 16:08:30 +0200

    Upgrades to the Ship.Upgrades class + full Ship javadoc

--- src/game/data/Ship.java
+++ src/game/data/Ship.java
@@ -31,33 +31,50 @@ import java.io.Serializable;
  * know about it.
  *
  * @author rudz
+ * @version 1.0
+ * @since 16-04-2016
  */
 public class Ship implements Serializable {
 
     private static final long serialVersionUID = -5311711410232026986L;
 
     /**
-     * The type of ship. Patrol = 2 length Destroyer & Submarine = 3 length
-     * Cruiser = 4 length Carrier = 5 length
+     * The type of possible ship types. Patrol = 2 length Destroyer & Submarine
+     * = 3 length Cruiser = 4 length Carrier = 5 length
      */
     public enum TYPE {
         PATROL, DESTROYER, SUBMARINE, CRUISER, CARRIER
     }
 
     /**
-     * The direction for which the ship is located, up-down or right-left
+     * The possible directions for the ship<br>
+     * HORIZONTAL = right-left<br>
+     * VERTICAL = up-down
      */
     public enum DIRECTION {
         HORIZONTAL, VERTICAL;
     }
 
+    /**
+     * The ship type
+     */
     private TYPE type;
+
+    /**
+     * The ship direction
+     */
     private DIRECTION direction;
 
+    /**
+     * The possible ship upgrades.
+     */
     public enum UPGRADES {
         ARMOR, SONAR, POWER
     }
 
+    /**
+     * The upgrades for the ship.
+     */
     private Upgrades upgrades;
 
     /**
@@ -81,14 +98,20 @@ public class Ship implements Serializable {
      */
     private boolean hasUpgrade;
 
-    
+    /**
+     * The basic hit index. 0 = not hit, 1 = hit
+     */
     private int[] hits;
-    
+
+    /**
+     * Default constructor.
+     */
     public Ship() {
     }
 
     /**
      * Constructor without upgrades defined.
+     *
      * @param x The X start location
      * @param y The Y start location
      * @param type The type of the ship
@@ -96,15 +119,18 @@ public class Ship implements Serializable {
      */
     public Ship(final int x, final int y, final TYPE type, final DIRECTION direction) {
         locStart = new PPoint(x, y);
+        length = getLen(type);
+        locEnd = setEnd(locStart, length, direction);
         this.type = type;
         this.direction = direction;
-        length = getLen(type);
         life = length;
         hits = new int[length];
+        upgrades = new Upgrades(); // just to make sure, java is picky with it's serialization!
     }
 
     /**
      * Constructor which contain upgrade(s)
+     *
      * @param x The X start location
      * @param y The Y start location
      * @param type The type of the ship
@@ -113,16 +139,19 @@ public class Ship implements Serializable {
      */
     public Ship(final int x, final int y, final TYPE type, final DIRECTION direction, final Upgrades upgrades) {
         this(x, y, type, direction);
-        this.upgrades = upgrades;
-        if (upgrades.armor > 0) { // add armor to life (mef mef)
-            life += upgrades.armor;
+        hasUpgrade = upgrades.hasUpgrades();
+        if (hasUpgrade) {
+            this.upgrades = new Upgrades(upgrades);
+            if (upgrades.armor > 0) {
+                life += upgrades.armor;
+            }
         }
     }
 
     /* upgrade helper functions */
-    
     /**
      * Adds an upgrade to the ship (if bought by the player)
+     *
      * @param upgradeType The upgrade type.
      */
     public void addUpgrade(final UPGRADES upgradeType) {
@@ -131,6 +160,7 @@ public class Ship implements Serializable {
 
     /**
      * Removes an upgrade from the ship (wahh, what why?)
+     *
      * @param upgradeType The upgrade type.
      */
     public void removeUpgrade(final UPGRADES upgradeType) {
@@ -138,15 +168,15 @@ public class Ship implements Serializable {
     }
 
     /* helper methods */
-    
     /**
      * Is the ship dead?
+     *
      * @return true if ship if dead, otherwise false
      */
     public boolean isDead() {
         return life == 0;
     }
-    
+
     /**
      * Hit the ship!! (argh!)
      */
@@ -155,39 +185,67 @@ public class Ship implements Serializable {
         hits[location] = 1;
     }
 
-    
-    public boolean isHit(int x, int y) {
-        return isHit((byte) x, (byte) y);
-    }
-    
+    /**
+     * Determines if the ship has been hit.<br>
+     * The ship will loose 1 life if hit.
+     *
+     * @param x The X coordinate to check
+     * @param y The Y coordinate to check
+     * @return true if ship is hit, otherwise false.
+     */
     public boolean isHit(byte x, byte y) {
         /* check off the bat for direct start & end hit first! */
         if (x == locStart.getX() && y == locStart.getY()) {
             hit(0);
             return true;
-        } else if (x == locEnd.getX()   && y == locEnd.getY()) {
+        } else if (x == locEnd.getX() && y == locEnd.getY()) {
             hit(length - 1);
             return true;
         }
-        
+
         // TODO :: WHAT WHAT!!!
         if (direction == DIRECTION.HORIZONTAL) {
             if (x + locStart.getX() < locEnd.getX()) {
-                
+                hit(locEnd.getX() - x);
+                return true;
             }
-        } else {
-            if (y + locStart.getY() < locEnd.getY()) {
-                
-            }
+        } else if (y + locStart.getY() < locEnd.getY()) {
+            hit(locEnd.getY() - y);
+            return true;
         }
         return false;
     }
 
-    private static PPoint setEnd(final Point start, final TYPE type, final DIRECTION direction) {
-        int len = 0;
-        return (direction == DIRECTION.HORIZONTAL) ? new PPoint(start.x + len, start.y) : new PPoint(start.x, start.y + len);
+    /**
+     * Overload of {@link #isHit(byte x, byte y)} to check for hit with
+     * integers.
+     *
+     * @param x The X coordinate to check
+     * @param y The Y coordinate to check
+     * @return true if ship is hit, otherwise false.
+     */
+    public boolean isHit(int x, int y) {
+        return isHit((byte) x, (byte) y);
     }
 
+    /**
+     * Set the end specified by the start and the type combined with direction.
+     *
+     * @param start The start PPoint object containing the start coordinates
+     * @param type The type of the ship
+     * @param direction The direction of the ship
+     * @return The end PPoint object
+     */
+    private static PPoint setEnd(final PPoint start, final int length, final DIRECTION direction) {
+        return (direction == DIRECTION.HORIZONTAL) ? new PPoint(start.getX() + length, start.getY()) : new PPoint(start.getX(), start.getY() + length);
+    }
+
+    /**
+     * Determines the length of the ship based on it's type.
+     *
+     * @param type The type of the ship
+     * @return The length of the ship
+     */
     private static int getLen(final TYPE type) {
         switch (type) {
             case DESTROYER:
@@ -198,7 +256,7 @@ public class Ship implements Serializable {
             case CRUISER:
                 return 4;
             default:
-                // patrol boat
+                // patruljepølse
                 return 2;
         }
     }
@@ -273,11 +331,47 @@ public class Ship implements Serializable {
 
         private static final long serialVersionUID = -6735966214424190784L;
 
+        /**
+         * Default constructor
+         */
         public Upgrades() {
         }
 
+        /**
+         * Constructs upgrade with specific amounts of upgrades
+         *
+         * @param armor The armor
+         * @param sonar The sonar
+         * @param power The power
+         */
+        public Upgrades(final int armor, final int sonar, final int power) {
+            this.armor = armor;
+            this.sonar = sonar;
+            this.power = power;
+        }
+
+        /**
+         * Constructs upgrade using supplied object
+         *
+         * @param upgrades The object to copy from
+         */
+        public Upgrades(final Upgrades upgrades) {
+            this(upgrades.armor, upgrades.sonar, upgrades.power);
+        }
+
+        /**
+         * The armor
+         */
         private int armor;
+
+        /**
+         * The sonar
+         */
         private int sonar;
+
+        /**
+         * The power
+         */
         private int power;
 
         @Override
@@ -285,6 +379,27 @@ public class Ship implements Serializable {
             return "Upgrades{" + "armor=" + armor + ", sonar=" + sonar + ", power=" + power + '}';
         }
 
+        /**
+         * Resets the upgrades to 0 (ZERO!)
+         */
+        public void reset() {
+            armor = sonar = power = 0;
+        }
+
+        /**
+         * Determine if the upgrade contains _any_ values.
+         *
+         * @return true if ship has at least one upgrade, otherwise false.
+         */
+        public boolean hasUpgrades() {
+            return armor + sonar + power > 0;
+        }
+
+        /**
+         * Add a single upgrade
+         *
+         * @param upgrade The upgrade type to add
+         */
         public void addUpgrade(final UPGRADES upgrade) {
             switch (upgrade) {
                 case ARMOR:
@@ -299,17 +414,50 @@ public class Ship implements Serializable {
             }
         }
 
+        /**
+         * Remove an upgrade
+         *
+         * @param upgrade The upgrade to remove
+         */
         public void removeUpgrade(final UPGRADES upgrade) {
             switch (upgrade) {
                 case ARMOR:
-                    armor--;
+                    if (armor > 0) {
+                        armor--;
+                    }
                     break;
                 case POWER:
-                    power--;
+                    if (power > 0) {
+                        power--;
+                    }
                     break;
                 case SONAR:
-                    sonar--;
+                    if (sonar > 0) {
+                        sonar--;
+                    }
                     break;
+            }
+        }
+
+        /**
+         * Adds a specified amount of upgrades of a specific type
+         *
+         * @param upgrade The upgrade type to add
+         * @param amount The amount of upgrades to add
+         */
+        public void addUpgrades(final UPGRADES upgrade, final int amount) {
+            if (amount > 0) {
+                switch (upgrade) {
+                    case ARMOR:
+                        armor += amount;
+                        break;
+                    case POWER:
+                        power += amount;
+                        break;
+                    case SONAR:
+                        sonar += amount;
+                        break;
+                }
             }
         }
 
